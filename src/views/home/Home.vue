7fc61c11457a682451c653e25cab5bb0e18af7bb
Jinkeee <872521525@qq.com>
2022-04-06 10:03:01 +0800

    商城首页

---
+++ src/views/home/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar> 
+    <home-swiper :banners="banners"/> 
+    <recommend-view :recommends="recommends"/> 
+    <feature-view/>
+    <ul>
+      
+      <li></li> 
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/common/navbar/NavBar.vue'
+import HomeSwiper from './childComponents/HomeSwiper.vue'
+import RecommendView from './childComponents/RecommendView.vue'
+import FeatureView from './childComponents/FeatureView.vue'
+import {getHomeMultidata} from 'network/home'
+
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    } 
+  }, 
+  created() {
+    //1.请求多个数据
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
+  }
+
+</script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+
+    position: sticky;
+    left: 0;
+    top: 0;
+
+    z-index: 9;
+  }
+</style>
